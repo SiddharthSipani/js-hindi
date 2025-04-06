12ce0ace2f3f53c53cb73b2147987da80bd119b7
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-06 05:52:56 +0000

    datatype,constant

---
+++ .devcontainer/01-basic/01_variable.js
@@ -0,0 +1,19 @@
+const account_Id="144553"
+let account_Email="sidd@gmail.com"
+var accountPassword ="123456"
+accountCity =-"jaipur"
+let accountState;
+
+//account_Id =2
+account_Email="git@gmail.com"
+accountPassword="2121"
+accountCity="bengular"
+
+/*
+Prefer not to  use var
+because of issue  in block Scope and functional scope
+
+*/
+
+console.log(account_Id);
+console.table([account_Id,accountPassword,account_Email,accountCity,accountState])
