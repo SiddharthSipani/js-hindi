1e054659e183a371c4cf8d072e2688309f75d8cb
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-06 12:08:20 +0000

    string

---
+++ .devcontainer/01-basic/06_string.js
@@ -0,0 +1,13 @@
+const name ="hitesh"
+const repocount= 50
+// console.log(name+repocount+"value");
+
+console.log(`Hello my name is ${name} and my repo count is${repocount}`);
+
+
+const gameName = new String (`hitesh`)
+
+console.log(gameName[0])
+
+log
+
