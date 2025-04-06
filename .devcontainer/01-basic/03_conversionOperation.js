d4db3e240a94fd8fc6afdecb83ded8b246cc8657
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-06 07:19:39 +0000

    comperision

--- .devcontainer/01-basic/03_conversionOperation.js
+++ .devcontainer/01-basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score =true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber =Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -17,7 +17,7 @@ let isLoggedIn ="sidd"
 
 
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 // 1=> true;0=>false
 //""=>false
 //"sidd"=true
@@ -25,6 +25,48 @@ console.log(booleanIsLoggedIn);
 let somenumber=33
 
 let stringnumber =String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//************************operation ****************************************
+
+let value=3
+let negValue =- value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hellow"
+let str2="hitesh"
+
+let str3=str1+str2
+console.log(str3);
+
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1" +2+2);
+// console.log((1+2)+"2"); 
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1= num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
 
---
+++ .devcontainer/01-basic/04_comperision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2-1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+/* we are try avoid this.*/
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined ==0);
+console.log(undefined<0);
+console.log(undefined>0);
+
+
+console.log("2"===2);
+
+
+
+
