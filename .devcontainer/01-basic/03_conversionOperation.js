a54ca01b193fb8fe591b62faaaa273d2cf4dc5dc
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-06 06:12:04 +0000

    conversion of datatype

---
+++ .devcontainer/01-basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score =true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+//"33"=>33
+//"33abs"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn ="sidd"
+
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true;0=>false
+//""=>false
+//"sidd"=true
+
+let somenumber=33
+
+let stringnumber =String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
