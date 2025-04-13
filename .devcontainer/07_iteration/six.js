86ceb9b022d8e3b54aa796496660f985f07c36a2
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-13 21:16:51 +0000

    re

---
+++ .devcontainer/07_iteration/six.js
@@ -0,0 +1,21 @@
+const coding =["js","ruby","java","python","cpp"]
+
+
+// const values = coding.forEach((element) => {
+//     console.log(element);
+//     return element
+
+    
+// });
+
+// console.log (values);
+
+const myNums =[ 1,2,3,4,5,6,7,8,9,10]
+
+// const newNums=myNums.filter((nums)=>{
+//     return nums>4
+
+// })
+//console.log(myNums);
+
+// console.log(newNums);
