a8915e744dce30dd32f6a3423b88bfb7ecd0e0c2
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-13 14:26:52 +0000

    iterage

---
+++ .devcontainer/07_iteration/one.js
@@ -0,0 +1,57 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) { 
+//     const element = [i];
+//     console.log(element);
+    
+    
+// }
+// //console.log(element);
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element==5) {
+//         console.log("element is  "+5)
+        
+//     }
+//     console.log(i)
+
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop values: ${i}`);
+    
+//     for (let j = 0; j < 10; j++) {
+//         //console.log(`Outer loop values: ${i}and inner loop ${j}`);
+//      console.log(i,'*'+j+'='+i*j);
+     
+        
+//     }
+    
+// // }
+
+// let myarray=["flash","batman","superman"]
+// console.log(myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+    
+    
+// }
+
+// break ,continuos
+
+
+for (let i = 0; i < 20; i++) {
+   //console.log(`value of i is ${i}`);
+   if(i ==5){
+    console.log(`Detect 5`);
+    continue
+    
+   }
+   console.log(`value of i is ${i}`);
+   
+   
+    
+}
