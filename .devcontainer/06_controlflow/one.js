c0519b1b9e82dbbd09db6c16a42c884b979da217
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-13 14:17:09 +0000

    container

---
+++ .devcontainer/06_controlflow/one.js
@@ -0,0 +1,55 @@
+// if
+ const isUserloggendIn =true
+ const Tempature =42
+if (2===2) {
+    console.log("exectude")
+
+
+}
+if (Tempature<43) {
+    console.log("less than 50")
+
+
+}
+console.log("Temprature is greater than 50")
+//<,>,<=,>=,==,!=
+
+// const power =200
+
+// if (score>50){
+//     const power ="fly"
+// console.log(`user power:${power}`);
+
+
+// }
+//console.log(`user power:${power}`);
+
+// const balance=1000
+// // if(balance>500) console.log("test"),console.log("test");
+
+// if (balance<500){
+//     console.log("lessthan");
+    
+// }else if (balance<750){
+//     console.log("lessthan 750");
+    
+// }else if (balance<1200){
+//     console.log("lessthan 1200");
+    
+// }
+
+const userLoggedIn=true
+const debitCard =true
+const loggedInFromGoogle =false
+const loggenedInFromEmail=true
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+    
+}
+
+
+if (loggedInFromGoogle || loggenedInFromEmail ){
+    console.log("user Logged in");
+    
+}
---
+++ .devcontainer/07_iteration/two.js
@@ -0,0 +1,24 @@
+//while
+
+let index =0
+while (index <=10){
+    console.log(`value of index is ${index}`);
+    index =index +2
+    
+
+
+}
+let myArray =['falsh',"batman","supperman"]
+
+let arr =0
+while (arr <myArray.length){
+    console.log((`Values is ${myArray[arr]}`));
+    arr =arr+1
+}
+
+let score =11
+
+do{
+  console.log (`score is ${score}`);
+  score++
+} while (score <=10);
