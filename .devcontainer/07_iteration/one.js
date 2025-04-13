e1363a59e343b32a14eff9d570a0cedc91b2db2e
SiddharthSipani <130305787+SiddharthSipani@users.noreply.github.com>
2025-04-13 13:12:06 +0000

    iteration

---
+++ .devcontainer/07_iteration/one.js
@@ -0,0 +1,17 @@
+// for loop
+
+for (let i = 0; i < 10; i++) { 
+    const element = [i];
+    console.log(element);
+    
+    
+}
+console.log(element);
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element==5) {
+        
+    }
+    
+}
